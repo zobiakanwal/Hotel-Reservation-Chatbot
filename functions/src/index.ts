ca63f86ec10e3d83de6715861bd92f9d62f41dab
Zobia Kanwal <xobiaaijaz@gmail.com>
2019-03-18 04:37:47 -0700

    done

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,51 +1,84 @@
 'use strict';
 
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 const { WebhookClient } = require('dialogflow-fulfillment');
-const { Carousel } = require('actions-on-google');
-//const { Card, Suggestion } = require('dialogflow-fulfillment');
 
-
-process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
-
-// URLs for images used in card rich responses
-
+process.env.DEBUG = 'dialogflow:*'; // enables lib debugging statements
+admin.initializeApp(functions.config().firebase);
+const db = admin.firestore();
 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
-  const _agent = new WebhookClient({ request, response });
-  console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
-  console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+    const _agent = new WebhookClient({ request, response });
+  //  const params = request.body.queryResult.parameters;
+    console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+    console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
 
-  const params = request.body.queryResult.parameters   //intializing parameters
-  const intentName = request.body.queryResult.intent.displayName   //getting parameters from dialogflow
-  console.log("Intent name", intentName);   
+    function welcome(agent) {
+        console.log('working');
+        agent.add(`Good day! Do you want to book a room  or have some feedback for us?`);
+    }
 
-  function welcome(agent) {
-    agent.add(`Welcome to my agent!`);
-  }
+    function fallback(agent) {
+        agent.add(`I'm sorry, I didn't understand. Can you say that again?`);
+    }
 
-  function fallback(agent) {
-    agent.add(`I didn't understand`);
-    agent.add(`I'm sorry, can you try again?`);
-  }
+    function roombooking(agent) {
 
-  function roombooking(agent) {
-    agent.add(`${params.name} your hotel booking request for ${params.RoomType}room is forwarded for 
-                ${params.persons}persons. We will contact you on ${params.email} soon`);
-  }
-  
-    function complaint(agent) {
-    agent.add(`Your feedback is duly noted against: \n Subject: ${params.subject}.
-                                 \n Description: ${params.description}`);
-  }
+        const firstname = agent.parameters.firstname;
+        const lastname = agent.parameters.lastname;
+        const persons = agent.parameters.persons;
+        const email = agent.parameters.email;
+        const RoomType = agent.parameters.RoomType;      
+        console.log(name, persons, email, RoomType);
 
-  // Run the proper handler based on the matched Dialogflow intent
-  const intentMap = new Map();
-  intentMap.set('Default Welcome Intent', welcome);
-  intentMap.set('Default Fallback Intent', fallback);
-  intentMap.set('RoomBooking', roombooking);
-  intentMap.set('Complaint', complaint);
-  
-  
-  _agent.handleRequest(intentMap);
+       const dialogflowAgentRef = db.collection('data').doc();   //.doc=docRef,('agent=anythin'=path)
+    
+
+        return db.runTransaction(t => {
+            t.set(dialogflowAgentRef, { entry: firstname, lastname, persons, email, RoomType});
+            return Promise.resolve('Write complete');
+        }).then(doc => {
+            agent.add(`${firstname} ${lastname} your hotel booking request for ${RoomType} room is forwarded for 
+      ${persons} persons. We will contact you on ${email} soon`);
+        }).catch(err => {
+            console.log(`Error writing to Firestore: ${err}`);
+            agent.add(`Failed to write on database`);
+        });
+
+    }
+
+
+     function complaint(agent) {
+
+        const typeFeedback = agent.parameters.typeFeedback;
+        const subject = agent.parameters.subject;
+        const description = agent.parameters.description;
+ 
+
+        const dialogflowAgentRef = db.collection('data').doc();   //.doc=docRef,('agent=anythin'=path)
+
+        return db.runTransaction(t => {
+            t.set(dialogflowAgentRef, { entry: typeFeedback, subject, description });
+            return Promise.resolve('Write complete');
+        }).then(doc => {
+            agent.add(`Your ${typeFeedback} is duly noted against: \n Subject: ${subject}.
+                \n Description: ${description}`);
+        }).catch(err => {
+            console.log(`Error writing to Firestore: ${err}`);
+            agent.add(`Failed to write on database`);
+        });
+
+    }
+    
+
+    // Run the proper handler based on the matched Dialogflow intent
+    const intentMap = new Map();
+    intentMap.set('Default Welcome Intent', welcome);
+    intentMap.set('Default Fallback Intent', fallback);
+    intentMap.set('RoomBooking', roombooking);
+    intentMap.set('Complaint', complaint);
+
+
+    _agent.handleRequest(intentMap);
 });
